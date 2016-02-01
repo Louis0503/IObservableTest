6884a7dfb31a84d7fd99332ccea4df2bf22bf412
LouisLiu <louis19820503@gmail.com>
2016-02-01 12:50:25 +0800

    Frist Commit

---
+++ IObservable/Location.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IObservable
+{
+    /// <summary>
+    /// Location Struct
+    /// </summary>
+    public struct Location
+    {
+        double lat, lon;
+        public Location(double latitude, double longitude)
+        {
+            this.lat = latitude;
+            this.lon = longitude;
+        }
+
+        public double Latitude
+        { get { return this.lat; } }
+
+        public double Longitude
+        { get { return this.lon; } }
+    }
+
+    /// <summary>
+    /// UnKnownLocation Exception
+    /// </summary>
+
+    public class LocationUnknownException : Exception
+    {
+        internal LocationUnknownException()
+        { }
+    }
+}
---
+++ IObservable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IObservable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Define a provider and two observers
+
+            LocationTracker provider = new LocationTracker();
+            LocationReporter reporter1 = new LocationReporter("FixGPS");
+            reporter1.Subscribe(provider);
+            LocationReporter reporter2 = new LocationReporter("MobileGPS");
+            reporter2.Subscribe(provider);
+            provider.TrackLocation(new Location(47.6456, -122.1312));
+            reporter1.Unsubscribe();
+            provider.TrackLocation(new Location(47.6677, -122.1199));
+            provider.TrackLocation(null);
+            provider.EndTransmission();
+            Console.ReadKey();
+        }
+    }
+}
